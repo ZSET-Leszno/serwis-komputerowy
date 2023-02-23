8f7b56512962bcc393e21bcd145ccea7af7fb2e4
Patryk Piekarski <pat4plol@gmail.com>
2023-02-23 20:00:50 +0100

    Zaktualizowano bazę danych

--- data/serwis_komputerowy-projekt.sql
+++ data/serwis_komputerowy-projekt.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Czas generowania: 11 Sty 2023, 16:16
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.1.6
+-- Host: localhost
+-- Czas generowania: 23 Lut 2023, 19:03
+-- Wersja serwera: 10.5.18-MariaDB-cll-lve
+-- Wersja PHP: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `serwis_komputerowy-projekt`
+-- Baza danych: `zset_wojcik`
 --
 
 -- --------------------------------------------------------
@@ -33,7 +33,7 @@ CREATE TABLE `czesci` (
   `cena` float NOT NULL,
   `ilosc` int(11) NOT NULL,
   `id_producenta` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -49,7 +49,21 @@ CREATE TABLE `klienci` (
   `miasto` varchar(32) NOT NULL,
   `adres` varchar(64) NOT NULL,
   `telefon` int(9) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `logowanie`
+--
+
+CREATE TABLE `logowanie` (
+  `id_konta` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `hasło` varchar(32) NOT NULL,
+  `id_klienta` int(11) DEFAULT NULL,
+  `id_pracownika` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -68,7 +82,7 @@ CREATE TABLE `pracownicy` (
   `posada` varchar(32) NOT NULL,
   `wyplata` float DEFAULT NULL,
   `premia` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -79,7 +93,7 @@ CREATE TABLE `pracownicy` (
 CREATE TABLE `producenci` (
   `id` int(11) NOT NULL,
   `nazwa` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -96,7 +110,7 @@ CREATE TABLE `reklamacje` (
   `data_przeslania` date DEFAULT NULL,
   `data_zwrotu` date DEFAULT NULL,
   `id_pracownika` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -106,10 +120,46 @@ CREATE TABLE `reklamacje` (
 
 CREATE TABLE `uslugi` (
   `id` int(11) NOT NULL,
-  `nazwa` int(11) NOT NULL,
+  `nazwa` varchar(32) DEFAULT NULL,
   `cena` int(11) NOT NULL,
   `id_czesci` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `uslugi`
+--
+
+INSERT INTO `uslugi` (`id`, `nazwa`, `cena`, `id_czesci`) VALUES
+(1, 'Instalacja systemu operacyjnego', 100, NULL),
+(2, 'Usuwanie wirusów', 50, NULL),
+(3, 'Naprawa zasilacza', 80, NULL),
+(4, 'Wymiana karty graficznej', 120, NULL),
+(5, 'Naprawa dysku twardego', 150, NULL),
+(6, 'Czyszczenie komputera', 30, NULL),
+(7, 'Odzyskiwanie danych', 200, NULL),
+(8, 'Instalacja antywirusa', 40, NULL),
+(9, 'Naprawa zasilania', 60, NULL),
+(10, 'Aktualizacja sterowników', 50, NULL),
+(11, 'Usuwanie programów szpiegujących', 60, NULL),
+(12, 'Optymalizacja systemu', 80, NULL),
+(13, 'Naprawa klawiatury', 40, NULL),
+(14, 'Zakup i instalacja nowego sprzęt', 100, NULL),
+(15, 'Konserwacja systemu', 50, NULL),
+(16, 'Ustawianie sieci', 90, NULL),
+(17, 'Naprawa systemu operacyjnego', 70, NULL),
+(18, 'Naprawa płyty głównej', 120, NULL),
+(19, 'Odzyskiwanie haseł', 100, NULL),
+(20, 'Naprawa monitora', 90, NULL),
+(21, 'Instalacja oprogramowania', 60, NULL),
+(22, 'Usuwanie niepotrzebnych plików', 30, NULL),
+(23, 'Naprawa przycisków', 50, NULL),
+(24, 'Usuwanie reklam', 40, NULL),
+(25, 'Naprawa złącza USB', 60, NULL),
+(26, 'Ustawianie antywirusa', 50, NULL),
+(27, 'Naprawa procesora', 100, NULL),
+(28, 'Czyszczenie klawiatury', 20, NULL),
+(29, 'Naprawa pamięci RAM', 80, NULL),
+(30, 'Naprawa gniazda ładowania', 70, NULL);
 
 -- --------------------------------------------------------
 
@@ -125,7 +175,7 @@ CREATE TABLE `zamowienia` (
   `czy_wykonano` tinyint(1) NOT NULL,
   `id_pracownika` varchar(11) DEFAULT NULL,
   `data_wykonania` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indeksy dla zrzutów tabel
@@ -143,6 +193,14 @@ ALTER TABLE `czesci`
 --
 ALTER TABLE `klienci`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `logowanie`
+--
+ALTER TABLE `logowanie`
+  ADD PRIMARY KEY (`id_konta`),
+  ADD KEY `id_pracownika/klienta` (`id_klienta`),
+  ADD KEY `id_pracownika` (`id_pracownika`);
 
 --
 -- Indeksy dla tabeli `pracownicy`
@@ -191,6 +249,12 @@ ALTER TABLE `klienci`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT dla tabeli `logowanie`
+--
+ALTER TABLE `logowanie`
+  MODIFY `id_konta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT dla tabeli `producenci`
 --
 ALTER TABLE `producenci`
@@ -217,6 +281,13 @@ ALTER TABLE `zamowienia`
 --
 ALTER TABLE `czesci`
   ADD CONSTRAINT `czesci_ibfk_1` FOREIGN KEY (`id_producenta`) REFERENCES `producenci` (`id`);
+
+--
+-- Ograniczenia dla tabeli `logowanie`
+--
+ALTER TABLE `logowanie`
+  ADD CONSTRAINT `logowanie_ibfk_1` FOREIGN KEY (`id_klienta`) REFERENCES `klienci` (`ID`),
+  ADD CONSTRAINT `logowanie_ibfk_2` FOREIGN KEY (`id_pracownika`) REFERENCES `pracownicy` (`PESEL`);
 
 --
 -- Ograniczenia dla tabeli `reklamacje`

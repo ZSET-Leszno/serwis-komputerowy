090f2d239f23813f24c972fa63d29042b8b452d4
Jakub Wójcik <wojcikjakub2003@gmail.com>
2023-03-16 11:24:51 +0100

    Admin.php prace remontowe

--- admin.php
+++ admin.php
@@ -41,123 +41,136 @@
 		</div>
 		
 		<br>
-		
-		<div id="sidebar" style="min-height: 430px;">
-			<a href="admin.php">
-				<div class="optionL">
-					<b>Strona główna</b>
-				</div>
-			</a>
+		<div id="subcontainer">
+			<div id="sidebar">
+				<a href="admin.php">
+					<div class="optionL">
+						<b>Strona główna</b>
+					</div>
+				</a>
 
-			<a href="">
-				<div class="optionL">
-					Baza danych
-				</div>
-			</a>
+				<a href="">
+					<div class="optionL">
+						Baza danych
+					</div>
+				</a>
 
-            <a href="">
-				<div class="optionL">
-					Zamówienia
-				</div>
-			</a>
+				<a href="">
+					<div class="optionL">
+						Zamówienia
+					</div>
+				</a>
 
-		</div>
-		
-		<div id="content" style="height: fit-content;">
-			<span class="bigtitle">Kwerendy</span>
+			</div>
 			
-			<div class="dottedline"></div>
-			<?php
-				$link = mysqli_connect("localhost", "zset_wojcik", "Wojcik_123", 'zset_wojcik');
-				if(!$link)
-				{
-					die("Błąd połączenia z bazą danych \n". mysqli_error($link));
-				}
+			<div id="content" style="height: fit-content;">
+				<span class="bigtitle">Kwerendy</span>
 				
-			?>
-			<form method="post" action="admin.php">
-			<input type="submit" name="kwerenda1" value="Użytkownicy" style="padding: 5px 10px;">
-			<?php
-				if(isset($_POST['kwerenda1']))
-				{
-					$query = "SELECT * FROM users;";
-					$result = mysqli_query($link, $query);
-					if (!$result)
+				<div class="dottedline"></div>
+				<?php
+					$link = mysqli_connect("localhost", "zset_wojcik", "Wojcik_123", 'zset_wojcik');
+					if(!$link)
 					{
-						echo("Błąd zapytania: ".mysqli_error($link));
-						exit();
+						die("Błąd połączenia z bazą danych \n". mysqli_error($link));
 					}
-					echo("<table id='kwerenda'>
-					<tr><th>id</th><th>Imię</th><th>Nazwisko</th><th>Miasto</th><th>Adres</th><th>Telefon</th><th>email</th><th>Uprawnienia</th>");
-					while ($row = mysqli_fetch_row($result))
-					{
-						echo "<tr>";
+					
+				?>
+				<form method="post" action="admin.php">
+				<input type="submit" name="kwerenda1" value="Użytkownicy" style="padding: 5px 10px;">
+				<input type="submit" name="kwerenda2" value="Partnerzy" style="padding: 5px 10px;">
+				<input type="submit" name="kwerenda3" value="Usługi" style="padding: 5px 10px;">
 
-						foreach($row as $i)
+				<?php
+					if(isset($_POST['kwerenda1']))
+					{
+						$query = "SELECT * FROM users;";
+						$result = mysqli_query($link, $query);
+						if (!$result)
 						{
-						echo("<td>".$i."</td>");
+							echo("Błąd zapytania: ".mysqli_error($link));
+							exit();
 						}
-						echo "</tr>";
+						echo("<table id='kwerenda'>
+						<tr><th>id</th><th>Imię</th><th>Nazwisko</th><th>Miasto</th><th>Adres</th><th>Telefon</th><th>email</th><th>Uprawnienia</th>");
+						while ($row = mysqli_fetch_row($result))
+						{
+							echo "<tr>";
+							$count = 0;
+							foreach($row as $i)
+							{
+								$count += 1;
+								if ($count == 8)
+								{
+									continue;
+								}
+								echo("<td>".$i."</td>");
+							}
+							echo "</tr>";
+						}
+						echo("</table>");
 					}
-					echo("</table>");
-				}
-			?>
-			<br>
-			<input type="submit" name="kwerenda2" value="Partnerzy" style="padding: 5px 10px;">
-			<?php
-				if(isset($_POST['kwerenda2']))
-				{
-					$query = "SELECT * FROM partners;";
-					$result = mysqli_query($link, $query);
-					if (!$result)
-					{
-						echo("Błąd zapytania: ".mysqli_error($link));
-						exit();
-					}
-					echo("<table>
-					<tr><th>id</th><th>Nazwa</th><th>Opis</th><th>Link</th>");
-					for($i = 0; $i < mysqli_num_rows($result); $i++)
-					{
-						echo("<tr>");
-						$echo = mysqli_fetch_row($result);
-						echo("<td>".$echo[0]."</td>");
-						echo("<td>".$echo[1]."</td>");
-						echo("<td>".$echo[2]."</td>");
-						echo("<td>".$echo[3]."</td>");
-						echo("</tr>");
-					}
-					echo("</table>");
-				}
-			?>
-			<br>
-			<input type="submit" name="kwerenda3" value="Usługi" style="padding: 5px 10px;">
-			<?php
-				if(isset($_POST['kwerenda3']))
-				{
-					$query = "SELECT * FROM services;";
-					$result = mysqli_query($link, $query);
-					if (!$result)
-					{
-						echo("Błąd zapytania: ".mysqli_error($link));
-						exit();
-					}
-					echo("<table>
-					<tr><th>id</th><th>Nazwa</th><th>Cena</th>");
-					for($i = 0; $i < mysqli_num_rows($result); $i++)
-					{
-						echo("<tr>");
-						$echo = mysqli_fetch_row($result);
-						echo("<td>".$echo[0]."</td>");
-						echo("<td>".$echo[1]."</td>");
-						echo("<td>".$echo[2]."</td>");
-						echo("</tr>");
-					}
-					echo("</table>");
-				}
-			?>
-			
+				?>
 
-			</form>
+				<?php
+					if(isset($_POST['kwerenda2']))
+					{
+						$query = "SELECT * FROM partners;";
+						$result = mysqli_query($link, $query);
+						if (!$result)
+						{
+							echo("Błąd zapytania: ".mysqli_error($link));
+							exit();
+						}
+						echo("<table>
+						<tr>
+							<th>id</th> <th>Nazwa</th> <th>Opis</th> <th>Link</th>
+						</tr>");
+						for($i = 0; $i < mysqli_num_rows($result); $i++)
+						while ($row = mysqli_fetch_row($result))
+						{
+							echo "<tr>";
+
+							foreach($row as $i)
+							{
+							echo("<td>".$i."</td>");
+							}
+							echo "</tr>";
+						}
+						echo("</table>");
+					}
+				?>
+
+				<?php
+					if(isset($_POST['kwerenda3']))
+					{
+						$query = "SELECT * FROM services;";
+						$result = mysqli_query($link, $query);
+						if (!$result)
+						{
+							echo("Błąd zapytania: ".mysqli_error($link));
+							exit();
+						}
+						echo("<table>
+						<tr>
+						<th>id</th> <th>Nazwa</th> <th>Cena</th>
+						</tr>");
+						while ($row = mysqli_fetch_row($result))
+						{
+							echo "<tr>";
+
+							foreach($row as $i)
+							{
+							echo("<td>".$i."</td>");
+							}
+							echo "</tr>";
+						}
+						echo("</table>");
+					}
+				?>
+				
+
+				</form>
+			</div>
 		</div>
 		<div id="footer">
 			© 2023 PCExpress Sp. z o.o. Wszelkie prawa zastrzeżone.
